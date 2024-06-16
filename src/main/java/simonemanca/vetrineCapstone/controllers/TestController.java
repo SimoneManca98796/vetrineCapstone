7b76c2f1ace6443a083eb55ae88db3d8faa90dc9
Simone Manca <simone.manca.98@gmail.com>
2024-06-16 20:44:13 +0200

    testing

--- src/main/java/simonemanca/vetrineCapstone/controllers/TestController.java
+++ src/main/java/simonemanca/vetrineCapstone/controllers/TestController.java
@@ -1,14 +1,31 @@
 package simonemanca.vetrineCapstone.controllers;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import simonemanca.vetrineCapstone.entities.User;
+import simonemanca.vetrineCapstone.repositories.UserRepository;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/test")
 public class TestController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "Test route funzionante!";
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/users")
+    public ResponseEntity<?> getAllUsers() {
+        try {
+            List<User> users = userRepository.findAll();
+            return ResponseEntity.ok(users);
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Errore durante il recupero degli utenti: " + ex.getMessage());
+        }
     }
 }
-
