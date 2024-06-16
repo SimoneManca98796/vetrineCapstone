1f56caab0d4517d222a504dfa291abbc221c8b89
Simone Manca <simone.manca.98@gmail.com>
2024-06-16 20:08:02 +0200

    +

--- src/main/java/simonemanca/vetrineCapstone/security/SecurityConfig.java
+++ src/main/java/simonemanca/vetrineCapstone/security/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                         )
                 )
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/test").permitAll()
                         .requestMatchers("/uploads/**").permitAll()
                         .requestMatchers("/favicon.ico").permitAll()
                         .requestMatchers("/static/**").permitAll()
