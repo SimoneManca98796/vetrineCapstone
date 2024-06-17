e98f0a656621fe37f6511c4cd25c215aefbe1811
Simone Manca <simone.manca.98@gmail.com>
2024-06-17 22:50:37 +0200

    aziende config

--- src/main/java/simonemanca/vetrineCapstone/security/SecurityConfig.java
+++ src/main/java/simonemanca/vetrineCapstone/security/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                         .requestMatchers("/webjars/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/prezzi/**").permitAll()
+                        .requestMatchers("/api/aziende/**").permitAll()
                         .requestMatchers("/").permitAll()
                         .anyRequest().authenticated());
 
